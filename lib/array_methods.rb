1556750ffe79613c0fb4c6d2346a2d7ccecdfa9d
Reese M Bowman <reesemb1@gmail.com>
2019-12-12 22:15:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,11 +3,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort
+  array.sort!
   return array[-1]
 end
 
 def find_min_value(array)
-  array.sort
+  array.sort!
   return array[0]
 end
